4eaabbd75362aa58f1ed12b741b8206011017c24
NicolasF425 <nifan42@gmail.com>
2025-04-18 17:34:21 +0200

    reorganisation des pages

--- litrevu/appli/views.py
+++ litrevu/appli/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from itertools import chain
 from . import forms, models
 from authentication.models import User
 
@@ -23,11 +24,37 @@ def flux(request):
         Q(user__in=followed)  # OU créées par les utilisateurs suivis
     ).order_by('-time_created')
 
+    posts = list(chain(tickets, reviews))
+    posts.sort(key=lambda x: x.time_created, reverse=True)
+
     context = {
-        'tickets': tickets,
-        'reviews': reviews,
+        'posts': posts,
     }
     return render(request, 'appli/flux.html', context=context)
+
+
+@login_required
+def post_list(request):
+    current_user = request.user
+    tickets = models.Ticket.objects.all()
+    reviews = models.Review.objects.all()
+
+    tickets = models.Ticket.objects.filter(
+        Q(user=current_user)   # Tickets créés par l'utilisateur connecté
+    ).order_by('-time_created')
+
+    reviews = models.Review.objects.filter(
+        Q(user=current_user)   # Reviews crééss par l'utilisateur connecté
+
+    ).order_by('-time_created')
+
+    posts = list(chain(tickets, reviews))
+    posts.sort(key=lambda x: x.time_created, reverse=True)
+
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'appli/post_list.html', context=context)
 
 
 @login_required
@@ -58,21 +85,24 @@ def ticket_list(request):
 def edit_ticket(request, ticket_id):
     ticket_form = forms.TicketForm()
     ticket = get_object_or_404(models.Ticket, id=ticket_id)
-    ticket_form = forms.TicketForm(instance=ticket)
-    if request.method == 'POST':
-        # handle the POST request here
-        ticket_form = forms.TicketForm(request.POST, instance=ticket)
-        if (ticket_form.is_valid()):
-            ticket = ticket_form.save(commit=False)
-            ticket.user = request.user
-            ticket_form.save()
+    if ticket.user == request.user:
+        ticket_form = forms.TicketForm(instance=ticket)
+        if request.method == 'POST':
+            # handle the POST request here
+            ticket_form = forms.TicketForm(request.POST, instance=ticket)
+            if (ticket_form.is_valid()):
+                ticket = ticket_form.save(commit=False)
+                ticket.user = request.user
+                ticket_form.save()
 
-            return redirect('flux')
+                return redirect('flux')
 
-    context = {
-        'ticket_form': ticket_form,
-    }
-    return render(request, 'appli/create_ticket.html', context=context)
+        context = {
+            'ticket_form': ticket_form,
+        }
+        return render(request, 'appli/create_ticket.html', context=context)
+    else:
+        return redirect('flux')
 
 
 @login_required
--- litrevu/litrevu/urls.py
+++ litrevu/litrevu/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path('review/<int:review_id>/delete/', appli.views.delete_review, name='delete_review'),
     path('response/<int:ticket_id>/', appli.views.create_response, name='response'),
     path('response/<int:response_id>/edit/', appli.views.edit_response, name='edit_response'),
+    path('post_list/', appli.views.post_list, name='post_list'),
     path('subscriptions/', appli.views.check_user_to_follow, name='subscriptions')
     ]
 if settings.DEBUG:
