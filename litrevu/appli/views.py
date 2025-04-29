505f9ec76a2de4ca4b0c4276299039bbe4aa16f3
NicolasF425 <nifan42@gmail.com>
2025-04-29 11:06:25 +0200

    correction requete review on abonnes de ticket utilisateur connecte

--- litrevu/appli/views.py
+++ litrevu/appli/views.py
@@ -64,13 +64,13 @@ def flux(request):
 
     tickets = models.Ticket.objects.filter(
         Q(user=current_user) |  # Tickets créés par l'utilisateur connecté
-        Q(user__in=followed) |  # OU créés par les utilisateurs suivis
-        Q(ticket__user=current_user)  # OU dont le ticket a été créé par l'utilisateur connecté
+        Q(user__in=followed)  # OU créés par les utilisateurs suivis
     ).order_by('-time_created')
 
     reviews = models.Review.objects.filter(
         Q(user=current_user) |  # Reviews créés par l'utilisateur connecté
-        Q(user__in=followed)  # OU créées par les utilisateurs suivis
+        Q(user__in=followed) |  # OU créées par les utilisateurs suivis
+        Q(ticket__user=current_user)  # OU dont le ticket a été créé par l'utilisateur connecté
     ).order_by('-time_created')
 
     # on fusionne les listes en posts et on les trie par ordre antichronologique
